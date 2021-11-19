a34fe46ee04176192c982c0bb4fe06417f6e0349
Alexandre Altgauzen <82180840+altgauzen@users.noreply.github.com>
2021-11-19 12:28:06 +0000

    desafio27 finalizado

---
+++ challenges/desafio27.js
@@ -0,0 +1,14 @@
+db.resumoVoos.insertOne(
+  { empresa: "PASSAREDO",
+    totalVoosDomesticos: db.voos.find({
+  $and: [ 
+    { "empresa.nome": "PASSAREDO" },
+    { natureza: "Doméstica" },
+  ],
+  }).count() },
+  );
+
+db.resumoVoos.findOne(
+  { empresa: "PASSAREDO" },
+  { _id: false, empresa: true, totalVoosDomesticos: true },
+);
